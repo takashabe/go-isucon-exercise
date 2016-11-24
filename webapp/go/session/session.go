d6bc6c9efaec3f37a1aae3bb3a9221dae056b467
takashabe <takashabe.public@gmail.com>
2016-11-24 23:30:19 +0900

    add session package

---
+++ webapp/go/session/session.go
@@ -0,0 +1,111 @@
+package session
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+
+	"github.com/takashabe/go-isucon-exercise/webapp/go/session"
+)
+
+type Manager struct {
+	provider    Provider
+	cookieName  string
+	maxLifeTime int64
+	lock        sync.Mutex
+}
+
+type Provider interface {
+	SessionInit(sid string) (Session, error)
+	SessionRead(sid string) (Session, error)
+	SessionDestroy(sid string) error
+	SessionGC(maxLifeTime int64)
+}
+
+type Session interface {
+	Set(key, value interface{}) error
+	Get(key interface{}) interface{}
+	Delete(key interface{}) error
+	SessionID() string
+}
+
+func NewManager(provideName, cookieName string, maxLifeTime int64) (*Manager, error) {
+	provider, ok := provides[provideName]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+	}
+	return &Manager{provider: provider, cookieName: cookieName, maxLifeTime: maxLifeTime}, nil
+}
+
+var globalSessions *session.Manager
+
+var provides = make(map[string]Provider)
+
+func Register(name string, provider Provider) {
+	if provider == nil {
+		panic("session: Register provider is nil")
+	}
+
+	if _, dup := provider[name]; dup {
+		panic("session: Register called twice for provide " + name)
+	}
+	provides[name] = provider
+}
+
+func (manager *Manager) sessionId() string {
+	b := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	return base64.URLEncoding.DecodeString(b)
+}
+
+func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		sid := manager.sessionId()
+		session, _ = manager.provider.SessionInit(sid)
+		cookie := http.Cookie{
+			Name:     manager.cookieName,
+			Value:    url.QueryEscape(uid),
+			Path:     "/",
+			HttpOnly: true,
+			MaxAge:   int(manager.maxLifeTime),
+		}
+		http.SetCookie(w, &cookie)
+	} else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ = manager.provider.SessionRead(sid)
+	}
+	return
+}
+
+func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) error {
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		return fmt.Errorf("session: invalid cookie")
+	}
+
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	manager.provider.SessionDestroy(cookie.Value)
+	// update cookie
+	cookie.Expires = time.Now()
+	cookie.MaxAge = -1
+	http.SetCookie(w, &cookie)
+}
+
+func (manager *Manager) GC() {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	manager.provider.SessionGC(manager.maxLifeTime)
+	time.AfterFunc(time.Duration(manager.maxLifeTime), func() { manager.GC() })
+}
