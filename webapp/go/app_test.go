ce8553532b09af6a5c4108ed8dcc0b735c5960c5
takashabe <takashabe.public@gmail.com>
2016-12-21 23:49:56 +0900

    add login test case.  but got error 'http: multiple response.WriteHeader calls'

--- webapp/go/app_test.go
+++ webapp/go/app_test.go
@@ -5,35 +5,29 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"testing"
-
-	"github.com/k0kubun/pp"
 )
 
-func mockServer(handler func(http.ResponseWriter, *http.Request)) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(handler))
-}
-
 func TestLoginWithGet(t *testing.T) {
-	server := mockServer(loginHandler)
+	server := httptest.NewServer(http.HandlerFunc(loginHandler))
 	defer server.Close()
 
 	res, err := http.Get(server.URL)
 	if err != nil {
 		t.Errorf("want no error, but %v", err.Error())
 	}
+	defer res.Body.Close()
 
 	expectedCode := 200
 	if res.StatusCode != expectedCode {
 		t.Errorf("want %d, but %d", expectedCode, res.StatusCode)
 	}
-
 	if len(res.Cookies()) != 0 {
 		t.Errorf("wont len 0, but len ", len(res.Cookies()))
 	}
 }
 
 func TestLoginWithPost(t *testing.T) {
-	server := mockServer(loginHandler)
+	server := httptest.NewServer(http.HandlerFunc(loginHandler))
 	defer server.Close()
 
 	client := &http.Client{
@@ -43,35 +37,30 @@ func TestLoginWithPost(t *testing.T) {
 	}
 
 	// Success Authenticate
-	auth := url.Values{}
-	auth.Add("email", "Doris@example.com")
-	auth.Add("password", "Doris")
-
+	auth := url.Values{
+		"email":    {"Doris@example.com"},
+		"password": {"Doris"},
+	}
 	res, err := client.PostForm(server.URL, auth)
 	if err != nil {
 		t.Errorf("want no error, but %v", err.Error())
 	}
 	if res.StatusCode != 302 {
-		t.Errorf("wont 302, but %d", res.StatusCode)
+		t.Errorf("want 302, but %d", res.StatusCode)
 	}
 	if loc, _ := res.Location(); loc.Path != "/" {
-		t.Errorf("wont /, but %v", loc.Path)
+		t.Errorf("want /, but %v", loc.Path)
 	}
-}
+	res.Body.Close()
 
-func TestLoginWithPost2(t *testing.T) {
-	server := mockServer(loginHandler)
-	defer server.Close()
-
-	// Fail Authenticate
-	// auth := url.Values{}
-	res, err := http.Get(server.URL)
-	// _, err := http.PostForm(server.URL, auth)
+	// Failure Authenticate
+	emptyAuth := url.Values{}
+	res, err = client.PostForm(server.URL, emptyAuth)
 	if err != nil {
 		t.Errorf("want no error, but %v", err.Error())
 	}
-	pp.Println(res)
-	// if res.StatusCode != 401 {
-	//   t.Errorf("wont 401, but %d", res.StatusCode)
-	// }
+	if res.StatusCode != 401 {
+		t.Errorf("want 401, but %d", res.StatusCode)
+	}
+	res.Body.Close()
 }
