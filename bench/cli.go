d74f58da95c333a4b726070018d6bb8c0d4f5867
takashabe <takashabe.public@gmail.com>
2017-02-13 19:05:04 +0900

    add bench entry point with CLI

---
+++ bench/cli.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	defaultHost = "localhost"
+	defaultPort = 80
+	defaultTime = 3 * 60 * 1000
+	defaultFile = "data/param.json"
+)
+
+// Exit codes. used only in Run()
+const (
+	ExitCodeOK = 0
+
+	// Specific error codes. begin 10-
+	ExitCodeError = 10 + iota
+	ExitCodeParseError
+)
+
+var (
+	// ErrParseFailed is failed to cli args parse
+	ErrParseFailed = errors.New("failed to parse args")
+)
+
+// PrintDebugf behaves like log.Printf only in the debug env
+func PrintDebugf(format string, args ...interface{}) {
+	if env := os.Getenv("ISUCON_BENCH_DEBUG"); len(env) != 0 {
+		log.Printf("[DEBUG] "+format+"\n", args...)
+	}
+}
+
+type param struct {
+	host string
+	port int
+	time int
+	file string
+}
+
+// CLI is the command line interface object
+type CLI struct {
+	outStream io.Writer
+	errStream io.Writer
+}
+
+// Run invokes the CLI with the given arguments
+func (c *CLI) Run(args []string) int {
+	param := &param{}
+	c.parseArgs(args, param)
+	return ExitCodeOK
+}
+
+func (c *CLI) parseArgs(args []string, p *param) error {
+	flags := flag.NewFlagSet("bench", flag.ContinueOnError)
+	flags.SetOutput(c.errStream)
+
+	flags.StringVar(&p.file, "file", defaultFile, "")
+	flags.IntVar(&p.port, "port", defaultPort, "")
+	flags.IntVar(&p.time, "time", defaultTime, "")
+	flags.StringVar(&p.host, "host", defaultHost, "")
+
+	err := flags.Parse(args)
+	if err != nil {
+		PrintDebugf("parse error: %v", err)
+		return errors.Wrapf(ErrParseFailed, err.Error())
+	}
+	return nil
+}
---
+++ bench/cli_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestParseArgs(t *testing.T) {
+	cli := &CLI{
+		outStream: os.Stdout,
+		errStream: os.Stderr,
+	}
+	cases := []struct {
+		input       string
+		expectErr   error
+		expectParam param
+	}{
+		{
+			"",
+			nil,
+			param{
+				file: defaultFile,
+				host: defaultHost,
+				port: defaultPort,
+				time: defaultTime,
+			},
+		},
+		{
+			"-port 8080 -host 127.0.0.1 -file data/test.json",
+			nil,
+			param{
+				host: "127.0.0.1",
+				port: 8080,
+				time: defaultTime,
+				file: "data/test.json",
+			},
+		},
+		{
+			"-nonparam foo",
+			ErrParseFailed,
+			param{},
+		},
+	}
+	for i, c := range cases {
+		p := &param{}
+		err := cli.parseArgs(strings.Split(c.input, " "), p)
+		if errors.Cause(err) != c.expectErr {
+			t.Errorf("#%d: want %#v, got %#v", i, c.expectErr, err)
+		}
+		if reflect.DeepEqual(p, c.expectParam) {
+			t.Errorf("#%d: want %d, got %d", i, c.expectParam, p)
+		}
+	}
+}
