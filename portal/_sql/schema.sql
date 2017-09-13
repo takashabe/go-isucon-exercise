dafe26e5ac922a77481c32d49f0c058ebe7f7f4f
takashabe <takashabe.public@gmail.com>
2017-09-13 19:23:53 +0900

    Add portal schema

---
+++ portal/_sql/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS teams (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `team` varchar(128) NOT NULL UNIQUE,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(128) NOT NULL UNIQUE,
+  `instance` varchar(128),
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS scores (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `team_id` int NOT NULL,
+  `summary` varchar(32) NOT NULL, -- success, fail
+  `score` int NOT NULL,
+  `submitted_at` timestamp,
+  `json` text
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS highscores (
+  `team_id` int NOT NULL PRIMARY KEY,
+  `score` int NOT NULL,
+  `submitted_at` timestamp
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS messages (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  -- http://getbootstrap.com/components/#alerts
+  `priority` varchar(16) DEFAULT 'alert-info', -- 'alert-success', 'alert-info', 'alert-warning', 'alert-danger'
+  `content` TEXT NOT NULL,
+  `show_at` timestamp NOT NULL,
+  `hide_at` timestamp NOT NULL
+) DEFAULT CHARSET=utf8mb4;
