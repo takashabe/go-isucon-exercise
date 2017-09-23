6bf1c58a67289910a961732c24fe77eb47a02978
takashabe <takashabe.public@gmail.com>
2017-09-23 19:42:30 +0900

    Add modules 'react', 'react-dom' and reinstall 'babel-loader'

---
+++ portal_frontend/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/app.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000,
+    inline: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
