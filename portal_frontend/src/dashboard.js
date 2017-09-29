09f0d71886971f0b88581102e69f4da8e0734627
takashabe <takashabe.public@gmail.com>
2017-09-29 13:50:39 +0900

    Add queues component

--- portal_frontend/src/dashboard.js
+++ portal_frontend/src/dashboard.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Dashboard extends React.Component {
   render() {
-    return <div>Hello Dashboard</div>;
+    return (
+      <div>
+        Hello Dashboard
+        <Queues />
+      </div>
+    );
+  }
+}
+
+class Queues extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeQueues: [],
+    };
+  }
+
+  componentWillMount() {
+    this.handleQueues();
+  }
+
+  handleQueues() {
+    axios
+      .get('/api/queues', {withCredentials: true})
+      .then(res => {
+        this.setState({
+          activeQueues: res.data,
+        });
+      })
+      .catch(e => {
+        console.log(JSON.stringify(e.response.data));
+      });
+  }
+
+  render() {
+    const style = {
+      myTeam: {
+        textDecoration: 'underline',
+      },
+    };
+    const queues = this.state.activeQueues.map(x => {
+      if (x.my_team) {
+        return (
+          <li key={x.message_id} style={style.myTeam}>
+            {x.message_id}
+          </li>
+        );
+      }
+      return <li key={x.message_id}>{x.message_id}</li>;
+    });
+    return <ul>{queues}</ul>;
   }
 }
