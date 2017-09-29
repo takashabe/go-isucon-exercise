7553caebeeb7ed44018c8d3db3b0e44133350c89
takashabe <takashabe.public@gmail.com>
2017-09-29 13:51:18 +0900

    Add call enqueue API

--- portal_frontend/src/enqueue.js
+++ portal_frontend/src/enqueue.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Enqueue extends React.Component {
+  componentWillMount() {
+    axios
+      .post('/api/enqueue', {withCredentials: true})
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => console.log(JSON.stringify(e.response.data)));
+  }
+
   render() {
     return <div>Enqueue</div>;
   }
