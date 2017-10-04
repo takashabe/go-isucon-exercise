834174d8167f64ea2f63450703559beee556650c
takashabe <takashabe.public@gmail.com>
2017-10-04 22:26:48 +0900

    Add dummy tuning files

---
+++ webapp/sql/index.sql
@@ -0,0 +1,8 @@
+-- user
+-- create index idx_email_and_passhash on user(email, passhash);
+
+-- follow
+--  create index idx_user_id_and_follow_id on follow(user_id, follow_id);
+
+-- tweet
+-- create index idx_user_id_and_created_at on tweet(user_id, created_at);
--- webapp/tools/index.sh
+++ webapp/tools/index.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/bin/cat ../sql/init.sql | mysql -uisucon isucon
+# /bin/cat ../sql/init.sql | mysql -uisucon isucon
 # /bin/cat ../sql/index.sql | mysql -uisucon isucon
