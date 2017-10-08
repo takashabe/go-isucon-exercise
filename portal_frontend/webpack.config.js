cf6ac6bffca70acddba898eed4d6e0efef7403f5
takashabe <takashabe.public@gmail.com>
2017-10-08 20:00:00 +0900

    Add loader for the normalize.css

--- portal_frontend/webpack.config.js
+++ portal_frontend/webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [{loader: 'style-loader'}, {loader: 'css-loader'}],
+      },
     ],
   },
 };
