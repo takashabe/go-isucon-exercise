2113f76c926b5dfb5173ffb10d588bd5acf64a0d
takashabe <takashabe.public@gmail.com>
2017-10-10 18:28:58 +0900

    Add team infomation to the summary

--- portal_frontend/src/summary.js
+++ portal_frontend/src/summary.js
@@ -8,6 +8,7 @@ import Table, {
   TableRow,
 } from 'material-ui/Table';
 import {withStyles} from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
 
 const styles = theme => ({
   root: {
@@ -15,6 +16,22 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
     marginLeft: 'auto',
     marginRight: 'auto',
+  },
+  paper: {
+    width: '100%',
+    marginTop: theme.spacing.unit,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    overflowX: 'auto',
+  },
+  paperTable: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  secondary: {
+    marginTop: theme.spacing.unit,
+    marginLeft: theme.spacing.unit,
   },
 });
 
@@ -30,7 +47,7 @@ class Summary extends React.Component {
       return (
         <TableRow>
           <TableCell padding="dense">{label}</TableCell>
-          <TableCell>{'failed to receive score data'}</TableCell>
+          <TableCell>Failed to receive score data</TableCell>
         </TableRow>
       );
     }
@@ -45,6 +62,8 @@ class Summary extends React.Component {
   }
 
   render() {
+    const {classes, team} = this.props;
+
     let highScore = null;
     let latestScore = null;
     for (const v of this.props.data) {
@@ -56,29 +75,68 @@ class Summary extends React.Component {
       }
     }
 
+    const teamRow =
+      team !== null ? (
+        <TableRow>
+          <TableCell>{team.ID}</TableCell>
+          <TableCell>{team.Name}</TableCell>
+          <TableCell>{team.Instance}</TableCell>
+        </TableRow>
+      ) : (
+        <TableRow>
+          <TableCell>Failed to receive team data</TableCell>
+        </TableRow>
+      );
+
     return (
-      <div className={this.props.classes.root}>
+      <div className={classes.root}>
         <Typography type="display1">Summary</Typography>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell />
-              <TableCell>Summary</TableCell>
-              <TableCell>Score</TableCell>
-              <TableCell>Timestamp</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.createScoreRow('High score', highScore)}
-            {this.createScoreRow('Latest score', latestScore)}
-          </TableBody>
-        </Table>
+        <Paper className={classes.paper}>
+          <Typography
+            className={classes.secondary}
+            color="secondary"
+            type="subheading">
+            Team detail
+          </Typography>
+          <Table className={classes.paperTable}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Team ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Instance</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{teamRow}</TableBody>
+          </Table>
+
+          <Typography
+            className={classes.secondary}
+            color="secondary"
+            type="subheading">
+            Highlight scores
+          </Typography>
+          <Table className={classes.paperTable}>
+            <TableHead>
+              <TableRow>
+                <TableCell />
+                <TableCell>Summary</TableCell>
+                <TableCell>Score</TableCell>
+                <TableCell>Timestamp</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.createScoreRow('High score', highScore)}
+              {this.createScoreRow('Latest score', latestScore)}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
 }
 
 Summary.propTypes = {
+  classes: PropTypes.object.isRequired,
   data: PropTypes.array.isRequired,
   team: PropTypes.object,
 };
